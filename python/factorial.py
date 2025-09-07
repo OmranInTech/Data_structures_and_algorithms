8a7d7861adab4bcd7a1d258b3fcd06db9c2836da
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-07 06:23:13 -0700

    Update factorial.py

--- python/factorial.py
+++ python/factorial.py
@@ -1,1 +1,17 @@
+def factorial (n):
+  """
+  this is factorial function use to find factorial of a number 
+  we use base case and recursive call to implement recursion concept
+  in practise.
+  """
+  if n == 0:
+    return 1
+  else : 
+    return n*factorial(n-1)
+
+#test
+number=3
+print(factorial(number))
+
+
 
