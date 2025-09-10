c34275845463b972b73724d4a58a41159df18e4d
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-10 06:57:17 -0700

    Update multiplication_table.py

--- logic_building/multiplication_table.py
+++ logic_building/multiplication_table.py
@@ -1,1 +1,8 @@
-
+def multiplication_table():
+    """Generate  a multiplication table."""
+    table_for=int(input("Enter the number for which you want the multiplication table: "))
+    range_for_table=int(input("Enter the range for the multiplication table: "))
+    for i in range(1,range_for_table+1):
+        print(f"{table_for} x {i} = {table_for*i}")
+#example usage :
+multiplication_table()
