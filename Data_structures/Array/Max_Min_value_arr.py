09ab847b97838b32ef5fbc3da386950d5637423c
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-10 09:21:52 -0700

    Update Max_Min_value_arr.py

--- Data_structures/Array/Max_Min_value_arr.py
+++ Data_structures/Array/Max_Min_value_arr.py
@@ -1,1 +1,14 @@
-
+def max_min_value_array(arr):
+    max_value = arr[0]
+    min_value = arr[0]
+    for i in arr:
+        if i > max_value:
+            max_value = i
+        if i < min_value:
+            min_value = i
+    return max_value, min_value
+# Example usage:
+arr = [3, 7, 1, 9, -2]
+max_val, min_val = max_min_value_array(arr)
+print("Max:", max_val)
+print("Min:", min_val)
