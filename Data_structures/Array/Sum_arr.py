d4d90211eb3bfc891a9bda457494d3dfdd54995f
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-10 09:31:00 -0700

    Update Sum_arr.py

--- Data_structures/Array/Sum_arr.py
+++ Data_structures/Array/Sum_arr.py
@@ -1,8 +1,9 @@
 def sum_arr(arr):
+  '''the function will give sum of all elements inside an array'''
   total=0
   for i in arr:
     total +=i
   return total
-
+#example usage
 arr=[1,2,3,4,5]
 print(sum_arr(arr))
