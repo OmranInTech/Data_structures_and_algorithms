0b324180064305d998d4627bd6c3f045187b11b1
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-11 07:34:52 -0700

    Update duplicates_in_arr.py

--- Data_structures/Array/duplicates_in_arr.py
+++ Data_structures/Array/duplicates_in_arr.py
@@ -1,9 +1,10 @@
 def duplicate_arr(arr):
+  '''this function remove duplicat values of array'''
   new_arr=[]
   for i in arr:
     if i not in new_arr:
       new_arr.append(i)
   return new_arr
-
+#example usage
 arr=[1,2,2,2,2,3,5]
 print(duplicate_arr(arr))
