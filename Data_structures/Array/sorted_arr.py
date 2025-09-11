8c07cd6aecbef2c59e79bceda1d96c6ed7f069f8
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-11 07:27:47 -0700

    Update sorted_arr.py

--- Data_structures/Array/sorted_arr.py
+++ Data_structures/Array/sorted_arr.py
@@ -1,1 +1,14 @@
-
+def sorted_arr(arr):
+  '''this function is use to check wether an array is sorted or not 
+  if sorted it will print True if not sorted it will print False'''
+  start=0
+  end=len(arr)-1
+  for i in range(start,end):
+    if arr[i] > arr[i+1]:
+      return False
+  return True
+#example usage
+arr=[1,2,3,4]
+arr_1=[2,4,3,1]
+print(sorted_arr(arr))
+print(sorted_arr(arr_1))
