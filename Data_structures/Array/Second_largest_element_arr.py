01dbac3d1b664dcdc93639e40c492c4dcc944ecd
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-11 07:02:10 -0700

    Update Second_largest_element_arr.py

--- Data_structures/Array/Second_largest_element_arr.py
+++ Data_structures/Array/Second_largest_element_arr.py
@@ -3,14 +3,12 @@ def second_largest(arr):
     for i in arr:
         if i > max_value:
             max_value = i
-
-    second_largest = None  # fixed variable name
+    second_largest = None  
     for i in arr:
         if i < max_value:
             if second_largest is None or i > second_largest:
                 second_largest = i
-
     return second_largest
-
+#exmaple usage
 arr = [1,2,3,4,5,6]
 print(second_largest(arr))
