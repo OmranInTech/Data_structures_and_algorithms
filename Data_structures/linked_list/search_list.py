e714c989686aefaa5e8bcdd38e31f24ac69e84c3
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-15 06:33:00 -0700

    Update search_list.py

--- Data_structures/linked_list/search_list.py
+++ Data_structures/linked_list/search_list.py
@@ -1,1 +1,17 @@
+node1=[10,None]
+node2=[20,None]
+node3=[30,None]
+node4=[40,None]
 
+node1[1]=node2
+node2[1]=node3
+node3[1]=node4
+
+head=node1
+
+def search_linkedlist(head):
+  current=head
+  while current is not None:
+    if current==head:
+      return ture
+  return false
