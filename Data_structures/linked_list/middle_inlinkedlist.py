76504216866674289ea9d96c734e0434e64e0c98
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-15 07:16:59 -0700

    Update middle_inlinkedlist.py

--- Data_structures/linked_list/middle_inlinkedlist.py
+++ Data_structures/linked_list/middle_inlinkedlist.py
@@ -1,1 +1,18 @@
 
+node1=[10,None]
+node2=[20,None]
+node3=[30,None]
+
+node1[1]=node2
+node2[1]=node3
+head=node1
+
+def middle(head):
+    slow=head
+    fast=head
+    while fast is not None and fast[1] is not None:
+        slow=slow[1]
+        fast=fast[1][1]
+    return slow[0]
+
+print(middle(head))  #20
