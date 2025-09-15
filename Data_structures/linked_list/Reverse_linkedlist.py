091c5eaa4c9aecf2cb8cb0adaac0e5eeb609b536
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-15 07:02:35 -0700

    Update Reverse_linkedlist.py

--- Data_structures/linked_list/Reverse_linkedlist.py
+++ Data_structures/linked_list/Reverse_linkedlist.py
@@ -1,1 +1,30 @@
 
+node1=[10,None]
+node2=[20,None]
+node3=[30,None]
+node4=[40,None]
+node5=[50,None]
+
+node1[1]=node2
+node2[1]=node3
+node3[1]=node4
+node4[1]=node5
+
+head=node1
+
+def reverse_linked_list(head):
+    prev=None
+    current=head
+    while current is not None:
+        next_node=current[1]
+        current[1]=prev
+        prev=current
+        current=next_node
+    return prev
+
+head=reverse_linked_list(head)
+current=head
+while current is not None:
+    print(current[0],end=' ')
+    current=current[1]
+print()
