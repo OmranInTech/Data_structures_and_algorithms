f8e6d635604cd355bc48d5171cf4b46e5a17273c
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-15 06:58:21 -0700

    Update Add_start_end_element.py

--- Data_structures/linked_list/Add_start_end_element.py
+++ Data_structures/linked_list/Add_start_end_element.py
@@ -1,1 +1,24 @@
+node1=[10,None]
+node2=[20,None]
+node3=[30,None]
 
+node1[1]=node2
+node2[1]=node3
+
+head=node1
+
+def add_at_end(head,value):
+  new_node=[value,None]
+  if head is None:
+    return head
+  current=head
+  while current is not None:
+    print(current[0])
+    current=current[1]
+  return head
+def add_at_start(head,value):
+  new_node=[value ,None]
+  return new_node
+head=add_at_end(head,40)
+head=add_at_start(head,5)
+  
