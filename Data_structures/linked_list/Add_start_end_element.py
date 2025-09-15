a3016330a278102d27cd1a5be6701e90a171a1f4
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-15 06:59:30 -0700

    Update Add_start_end_element.py

--- Data_structures/linked_list/Add_start_end_element.py
+++ Data_structures/linked_list/Add_start_end_element.py
@@ -1,24 +1,45 @@
-node1=[10,None]
-node2=[20,None]
-node3=[30,None]
-
-node1[1]=node2
-node2[1]=node3
-
-head=node1
-
-def add_at_end(head,value):
-  new_node=[value,None]
-  if head is None:
+def add_at_end(head, value):
+    new_node = [value, None]
+    
+    # If list is empty, new node becomes head
+    if head is None:
+        return new_node
+    
+    current = head
+    while current[1] is not None:  # traverse till last node
+        current = current[1]
+    current[1] = new_node  # link last node to new node
+    
     return head
-  current=head
-  while current is not None:
-    print(current[0])
-    current=current[1]
-  return head
-def add_at_start(head,value):
-  new_node=[value ,None]
-  return new_node
-head=add_at_end(head,40)
-head=add_at_start(head,5)
-  
+
+def add_at_start(head, value):
+    new_node = [value, head]  # new node points to current head
+    return new_node  # new node becomes the new head
+
+def print_list(head):
+    current = head
+    while current is not None:
+        print(current[0], end=' -> ')
+        current = current[1]
+    print('None')
+
+# Example usage
+node1 = [10, None]
+node2 = [20, None]
+node3 = [30, None]
+
+node1[1] = node2
+node2[1] = node3
+
+head = node1
+
+print("Original list:")
+print_list(head)
+
+head = add_at_end(head, 40)
+print("\nAfter adding 40 at end:")
+print_list(head)
+
+head = add_at_start(head, 5)
+print("\nAfter adding 5 at start:")
+print_list(head)
