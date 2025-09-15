92bad99bb8ec678e08284eba7a7f1082484c737f
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-15 06:28:51 -0700

    Update length_of_list.py

--- Data_structures/linked_list/length_of_list.py
+++ Data_structures/linked_list/length_of_list.py
@@ -1,3 +1,16 @@
 node1=[10,None]
 node2=[20,None]
 node3=[30,None]
+
+node1[1]=node2
+node2[1]=node3
+
+head=node1
+
+def lenght_linkedlist(head):
+  count=0
+  current=head
+  while current in not None:
+    count +=1
+    current[0]=current[1]
+  return count
