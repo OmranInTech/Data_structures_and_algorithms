70f49ea333a0a062110e06d8645333a2f60e0d47
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-16 05:19:27 -0700

    Update middle_inlinkedlist.py

--- Data_structures/linked_list/middle_inlinkedlist.py
+++ Data_structures/linked_list/middle_inlinkedlist.py
@@ -1,18 +1,17 @@
-
+#middle element of linkedlist
 node1=[10,None]
 node2=[20,None]
 node3=[30,None]
-
 node1[1]=node2
 node2[1]=node3
 head=node1
-
 def middle(head):
+    '''function for finding middle element'''
     slow=head
     fast=head
     while fast is not None and fast[1] is not None:
         slow=slow[1]
         fast=fast[1][1]
     return slow[0]
-
+#example usage
 print(middle(head))  #20
