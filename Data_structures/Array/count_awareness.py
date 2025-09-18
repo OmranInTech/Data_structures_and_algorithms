39ccf7417a063bfd2a2ce8f995316e845f1ea84b
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-18 07:46:51 -0700

    Update count_awareness.py

--- Data_structures/Array/count_awareness.py
+++ Data_structures/Array/count_awareness.py
@@ -5,6 +5,6 @@ def count_awarness(arr,n):
     if i == n :
       total +=1
   return total 
-#example usage 
+#example usage of function
 arr=[1,2,3,4,5,5,6,5,5]
 print(count_awarness(arr,5))#output is 4
