818318c428bb272e34044230d45765c423fe9274
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-20 07:08:18 -0700

    Update subarray_sum.py

--- Data_structures/Array/subarray_sum.py
+++ Data_structures/Array/subarray_sum.py
@@ -1,1 +1,12 @@
+#subarray sum 
+def subarray_sum(arr,k):
+  count=0
+  n=len(arr)
+  for i in range(n):
+    total=0
+    for j in range(i,n):
+      total +=arr[j]
+      if total == k:
+        count+=1
+  return count 
 
