23c4876000716462d6e0a59013d5a341ab889edc
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-22 08:02:10 -0700

    Update sum_of_naturals.py

--- logic_building/sum_of_naturals.py
+++ logic_building/sum_of_naturals.py
@@ -1,6 +1,6 @@
 def sum_of_naturals(n):
   ''' this function will return the same of natural numbers give by a user'''
-  if n>1:
+  if n<=0:
     return 'please input number greaterthan zero!'
   elif n==1:
     return 1
