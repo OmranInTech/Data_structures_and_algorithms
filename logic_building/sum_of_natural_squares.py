10fd7a2f671c0edfdc5ed49e3f9b77b0731c47dc
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-22 08:18:40 -0700

    Update sum_of_natural_squares.py

--- logic_building/sum_of_natural_squares.py
+++ logic_building/sum_of_natural_squares.py
@@ -1,1 +1,10 @@
 
+def sumofnaturalsquares(n):
+    """Returns the sum of the squares of the first n natural numbers."""
+    if n < 1:
+        return 0
+    else:
+        return n**2 + sumofnaturalsquares(n - 1)
+
+  #exmaple usage 
+  print(sumofnaturalsqaures(12))
