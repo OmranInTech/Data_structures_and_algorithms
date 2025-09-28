2c740bfcab36f14a6edd3eaac61e66d5c4cd159f
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-28 06:21:40 -0700

    Update Reverse_string.py

--- Data_structures/Stack/Reverse_string.py
+++ Data_structures/Stack/Reverse_string.py
@@ -5,7 +5,6 @@ stack = []
 # Push all characters onto the stack
 for char in s:
     stack.append(char)
-# Pop characters to reverse the string
 reversed_s = ""
 while stack:
     reversed_s += stack.pop()
