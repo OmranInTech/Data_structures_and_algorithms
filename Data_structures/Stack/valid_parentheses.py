6fe4dfef0bbc46a8aa13f4abe96a410ff24c097f
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-10-01 08:55:58 -0700

    Update valid_parentheses.py

--- Data_structures/Stack/valid_parentheses.py
+++ Data_structures/Stack/valid_parentheses.py
@@ -1,1 +1,19 @@
-
+def valid_parentheses(s):
+  #this function will help us to find the valid parentheses
+  stack=[]
+  parents={
+  ')':'(',
+  '}':'{',
+  ']':'['
+  }
+  for char in s:
+    if char in parents:
+      top=stack.pop() if stack else '#'
+      if parents[char] != top:
+        return False
+    else:
+      stack.append(char)
+  return not stack
+#example usage 
+s='()[]{}'
+print(valid_parentheses(s))
