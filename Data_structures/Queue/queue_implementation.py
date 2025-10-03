85133a10053e03bd96224a0d90b8c5ad4291e65e
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-10-03 06:38:08 -0700

    Update queue_implementation.py

--- Data_structures/Queue/queue_implementation.py
+++ Data_structures/Queue/queue_implementation.py
@@ -1,16 +1,11 @@
 queue=[]
-
 def enqueue(item):
     queue.append(item)
     print('enqueueed an item :' ,{item})
-
 def dequeue():
     return queue.pop(0)
-
 def peek():
     return queue[0]
-
-
 enqueue(12)
 enqueue(14)
 enqueue(66)
