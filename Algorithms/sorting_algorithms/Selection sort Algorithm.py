342c141a7a41c82c713108507e59948b9ef79f76
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-10-06 04:49:10 -0700

    Update Selection sort Algorithm.py

--- Algorithms/sorting_algorithms/Selection sort Algorithm.py
+++ Algorithms/sorting_algorithms/Selection sort Algorithm.py
@@ -1,39 +1,27 @@
-#selection sort algorithm
+def selection_sort(arr):
+    """
+    Sorts a list in ascending order using the Selection Sort algorithm.
 
-#selection sort function                                
-def selectionsort (arr):
-   
-   #declare the length of array
+    Parameters:
+    arr (list): The list of numbers to sort.
+
+    Returns:
+    list: Sorted list in ascending order.
+
+    Notes:
+    In-place algorithm: modifies the original list.
+    """
     n = len(arr)
-    
-    #perfom loop on each element in array
     for i in range(n):
-        
-        #value index equals to minimum
-        min_index=i
-        
-        #value index i will be comapred with other values in array
-        for j in range(i+1,n):
-            
-            #if smaller value was found than index i value 
+        min_index = i  # Assume current index is minimum
+        for j in range(i+1, n):
             if arr[j] < arr[min_index]:
-                
-                #so  new smaller value will be updated as minimum value
-                min_index=j
-
-        #now we will store small value in temp varibale
-        temp=arr[min_index]
-        #now we will asign small value to index i 
-        arr[min_index]=arr[i]
-        #now we will assign un sorted value in index i to minindex position
-        arr[i]=temp
-
-        #printin steps for better understanding
-        print(f'step {i+1}: {arr}')
-
-    #return arr    
+                min_index = j  # Found new minimum
+        arr[i], arr[min_index] = arr[min_index], arr[i]  # Swap
     return arr
 
-#example for function
-array=[61,25,12,22,11]
-print(selectionsort(array))
+if __name__ == "__main__":
+    arr = [64, 25, 12, 22, 11]
+    print("Original array:", arr)
+    sorted_arr = selection_sort(arr)
+    print("Sorted array:", sorted_arr)
