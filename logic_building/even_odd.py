fe574f44291de575e1411bba475c00fe2eaeb12f
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-11-20 08:31:17 -0800

    Update even_odd.py

--- logic_building/even_odd.py
+++ logic_building/even_odd.py
@@ -1,10 +1,8 @@
 def even_odd(n):
-    """Return 'Even' if n is even, 'Odd' if n is odd."""
     if n % 2 == 0:
         return "Even"
     else:
         return "Odd"
-# Example usage:
 print(even_odd(4))  # Output: Even
 print(even_odd(7))  # Output: Odd
 
